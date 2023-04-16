a7e6f8ae1570f710e6084b1371f54564737885a6
El mehdi Elmohammady <eelmoham@e2r9p9.1337.ma>
2023-04-16 08:32:59 +0000

    sending files

--- src/bot.cpp
+++ src/bot.cpp
@@ -161,3 +161,13 @@ bool Server::irc_bot(int fd, s_command &c)
     }
     return false;
 }
+
+
+bool Server::irc_send(int fd, s_command &c)
+{
+    if (c.target.size() == 0 || c.first_pram.size() == 0 || c.second_pram.size() == 0)
+    {
+        
+        return (false);
+    }
+}
